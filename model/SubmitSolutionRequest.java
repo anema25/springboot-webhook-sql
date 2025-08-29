4c7b1357035398ea1be8288b1b210fe4b335038b
anema25 <116646218+anema25@users.noreply.github.com>
2025-08-29 10:39:17 +0530

    Create SubmitSolutionRequest.java

---
+++ model/SubmitSolutionRequest.java
@@ -0,0 +1,11 @@
+package com.example.hiring.model;
+
+public class SubmitSolutionRequest {
+  private String finalQuery;
+
+  public SubmitSolutionRequest() {}
+  public SubmitSolutionRequest(String finalQuery) { this.finalQuery = finalQuery; }
+
+  public String getFinalQuery() { return finalQuery; }
+  public void setFinalQuery(String finalQuery) { this.finalQuery = finalQuery; }
+}
